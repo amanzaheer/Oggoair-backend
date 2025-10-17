36c95f9239630dbc9823d1cbc51bda6a5211e0b4
amanzaheer <amanzaheer0808@gmail.com>
2025-10-17 17:47:49 +0500

    fix(duffel): add dotenv config to duffelClient for proper env loading

--- utils/duffelClient.js
+++ utils/duffelClient.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+require('dotenv').config();
 
 const DUFFEL_BASE_URL = process.env.DUFFEL_BASE_URL || 'https://api.duffel.com';
 const DUFFEL_VERSION = process.env.DUFFEL_VERSION || 'v1';
